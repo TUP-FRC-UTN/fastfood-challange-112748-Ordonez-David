1136ab88431254734b80e8870e2e8054b8c153e6
112748_Ordoñez_David <112748@tecnicatura.frc.utn.edu.ar>
2024-09-19 11:03:46 -0300

    Listo

--- fastfoodChallenge/src/ready-orders/ready-orders.component.ts
+++ fastfoodChallenge/src/ready-orders/ready-orders.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Order } from '../app/order';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ready-orders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ready-orders.component.html',
   styleUrl: './ready-orders.component.css'
 })
 export class ReadyOrdersComponent {
+
+  @Input() ordersReady: Order[] = []; // Recibir la lista de órdenes desde el componente padre
+
 
 }
